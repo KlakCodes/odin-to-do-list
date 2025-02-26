f75b1c17f3763b7af082758b2c77564fa7409643
KlakCodes <harrisonjclark@hotmail.com>
2025-02-26 15:31:29 +0000

    Continued work on the UI

--- src/DOM.js
+++ src/DOM.js
@@ -4,9 +4,20 @@ const DOM = (() => {
         projectContainer.innerHTML = "";
 
         projects.forEach(project => {
-            const projectElement = document.createElement("div");
+            const projectElement = document.createElement("li");
             projectElement.textContent = project.title;
             projectContainer.appendChild(projectElement);
+        });
+    }
+
+    function renderTasks(project) {
+        const taskContainer = document.querySelector(".tasks");
+        taskContainer.innerHTML = "";
+
+        project.tasks.forEach(task => {
+            const taskElement = document.createElement("div");
+            taskElement.textContent = task.title;
+            taskContainer.appendChild(taskElement);
         });
     }
 
@@ -17,6 +28,16 @@ const DOM = (() => {
         testMessage.textContent = "This is a test message in a function!";
         body.appendChild(testMessage);
     }
+
+    const newProjectBtn = document.querySelector("#newProjectBtn");
+    newProjectBtn.addEventListener("click", () => {
+        console.log("New project button clicked!");
+    });
+
+    const newTaskBtn = document.querySelector("#newTaskBtn");
+    newTaskBtn.addEventListener("click", () => {
+        console.log("New task button clicked!");
+    });
     // TEST END
 
     return { renderProjects, renderUI };
